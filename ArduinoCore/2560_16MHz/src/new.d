3491a85f312eb0c8b82041fb081ccd5838122813
Michael Kaufmann <michael@michael-Keller-Server.(none)>
2013-09-30 23:14:23 +0200

    Initial checkin

---
+++ ArduinoCore/2560_16MHz/src/new.d
@@ -0,0 +1,4 @@
+src/new.d src/new.o: ../src/new.cpp \
+ /home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/new.h
+
+/home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/new.h:
---
+++ ArduinoCore/328P_16MHz/src/main.d
@@ -0,0 +1,31 @@
+src/main.d src/main.o: ../src/main.cpp \
+ /home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/Arduino.h \
+ /home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/binary.h \
+ /home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/WCharacter.h \
+ /home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/WString.h \
+ /home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/HardwareSerial.h \
+ /home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/Stream.h \
+ /home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/Print.h \
+ /home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/Printable.h \
+ /home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/new.h \
+ /home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/pins_arduino.h
+
+/home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/Arduino.h:
+
+/home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/binary.h:
+
+/home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/WCharacter.h:
+
+/home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/WString.h:
+
+/home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/HardwareSerial.h:
+
+/home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/Stream.h:
+
+/home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/Print.h:
+
+/home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/Printable.h:
+
+/home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/new.h:
+
+/home/michael/eclipse-workspace/Arduino-Blink-Eclipse-Project/ArduinoCore/src/pins_arduino.h:
